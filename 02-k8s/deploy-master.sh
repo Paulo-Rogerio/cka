91ae3cedddf68985525d4ff6ba0258b163dae41c
Paulo Rogerio Gomes e Silva <prgomessilva@tjgo.jus.br>
2026-02-01 16:03:26 -0300

    Add script para deploy cluster

---
+++ 02-k8s/deploy-master.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)
+
+source ./01-upgrade.sh
+source ./02-common.sh
+source ./master/03-init.sh
+source ./master/04-network.sh
+source ./master/05-taint.sh
+source ./master/06-metallb.sh
+source ./master/07-metrics-server.sh
---
+++ 02-k8s/deploy-worker.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)
+
+source ./01-upgrade.sh
+source ./02-common.sh
+source ./worker/03-kubeconfig.sh
+source ./worker/04-join.sh
+source ./worker/05-label.sh
