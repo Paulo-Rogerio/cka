1b8a286f25d9e20ac95a84cb28548d079d0aa47b
Paulo Rogerio Gomes e Silva <prgomessilva@tjgo.jus.br>
2026-02-01 10:30:08 -0300

    Join member cluster

--- 01-setup-cluster/02-k8s/worker/05-label.sh
+++ 01-setup-cluster/02-k8s/worker/05-label.sh
@@ -4,5 +4,10 @@ echo "***********************************************************************"
 echo "* Definir Label nos Workers                                           *"
 echo "***********************************************************************"
 echo
-node=$(kubectl get nodes --no-headers | awk '$3=="<none>" {print $1}')
-kubectl label node ${node} node-role.kubernetes.io/worker=""
+
+nodes=$(kubectl get nodes --no-headers | awk '$3 == "<none>" {print $1}')
+
+for i in ${nodes[@]}
+do
+  kubectl label node ${i} node-role.kubernetes.io/worker=""
+done
