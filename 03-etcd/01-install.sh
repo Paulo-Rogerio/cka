b52df32a1047cfef3401e3bffd8b121b891a90e5
Paulo Rogerio Gomes e Silva <prgomessilva@tjgo.jus.br>
2026-02-01 12:25:08 -0300

    Estudos Etcd

--- 03-etcd/01-install.sh
+++ 03-etcd/01-install.sh
@@ -12,10 +12,10 @@ export ETCD_VERSION=$(curl -sSL https://api.github.com/repos/etcd-io/etcd/releas
 export DOWNLOAD_URL="https://github.com/etcd-io/etcd/releases/download"
 
 curl -sSL ${DOWNLOAD_URL}/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VERSION}-linux-amd64.tar.gz
-tar xzvf /tmp/etcd-${ETCD_VERSION}-linux-amd64.tar.gz -C /tmp/etcd-download-test --strip-components=1
+tar xzvf /tmp/etcd-${ETCD_VERSION}-linux-amd64.tar.gz -C /tmp/etcd-download --strip-components=1
 
 rm -f /tmp/etcd-${ETCD_VERSION}-linux-amd64.tar.gz
-mv /tmp/etcd-download-test/etcd* /usr/local/bin
+mv /tmp/etcd-download/etcd* /usr/local/bin
 
 curl -sSL -o /usr/local/bin/cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
 curl -sSL -o /usr/local/bin/cfssljson https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
