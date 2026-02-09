05530e1ac9bc952f008242e6ab6088b98eb61232
Paulo Rogerio Gomes e Silva <prgomessilva@tjgo.jus.br>
2026-02-09 06:06:28 -0300

    Add novo membro etcd

--- CKA/02-etcd/etcd-manager/cluster/staticPod/06-certificates.sh
+++ CKA/02-etcd/etcd-manager/cluster/staticPod/06-certificates.sh
@@ -3,6 +3,10 @@
 
 HOSTS=("master03")
 
+mkdir -p /etc/kubernetes/pki/etcd/
+scp -r root@master01:/etc/kubernetes/pki/etcd/ca.crt /etc/kubernetes/pki/etcd/ca.crt
+scp -r root@master01:/etc/kubernetes/pki/etcd/ca.key /etc/kubernetes/pki/etcd/ca.key
+
 for i in ${HOSTS[@]}
 do
   kubeadm init phase certs etcd-server --config=/root/kubeadmcfg-etcd/${i}/kubeadmcfg.yaml
@@ -10,7 +14,4 @@ do
   kubeadm init phase certs etcd-healthcheck-client --config=/root/kubeadmcfg-etcd/${i}/kubeadmcfg.yaml
   kubeadm init phase certs apiserver-etcd-client --config=/root/kubeadmcfg-etcd/${i}/kubeadmcfg.yaml
   mkdir -p /root/kubeadmcfg-etcd/${i}/certs
-  scp -r root@master01:/etc/kubernetes/pki/etcd/ca.crt /root/kubeadmcfg-etcd/${i}/certs
-  scp -r root@master01:/etc/kubernetes/pki/etcd/ca.key /root/kubeadmcfg-etcd/${i}/certs
-  find /etc/kubernetes/pki -type f -delete
 done
