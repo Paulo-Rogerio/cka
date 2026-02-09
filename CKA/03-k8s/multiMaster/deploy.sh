b3f0f011e594da2b59407a371d6e70ca6d58a313
Paulo Rogerio Gomes e Silva <prgomessilva@tjgo.jus.br>
2026-02-09 04:56:56 -0300

    Rename Folder

---
+++ CKA/03-k8s/multiMaster/deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)
+
+
+if [[ $(hostname -s) == "master01" ]]
+then
+  source ./01-common.sh
+  source ./master/vip.sh
+  source ./02-init.sh
+  source ../deployments/01-cni.sh
+  source ../deployments/02-taint.sh
+  source ../deployments/03-metallb.sh
+  source ../deployments/04-metrics-server.sh
+else
+  source ./01-common.sh
+  source ./02-init.sh
+  source ../deployments/01-cni.sh
+fi
+
+bash
