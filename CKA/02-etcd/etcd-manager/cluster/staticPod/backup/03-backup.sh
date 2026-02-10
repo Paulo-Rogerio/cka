383bb96e8d9c43a8e8a6f51efce41dbac4f94a64
Paulo Rogerio Gomes e Silva <prgomessilva@tjgo.jus.br>
2026-02-10 05:05:34 -0300

    Backup

--- CKA/02-etcd/etcd-manager/cluster/staticPod/backup/03-backup.sh
+++ CKA/02-etcd/etcd-manager/cluster/staticPod/backup/03-backup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cat > /root/etcdctl.env <<EOF
-export ETCDCTL_ENDPOINTS=https://master01:2379,https://master03:2379
+export ETCDCTL_ENDPOINTS=https://master03:2379
 export ETCDCTL_CACERT=/etc/kubernetes/pki/etcd/ca.crt
 export ETCDCTL_CERT=/etc/kubernetes/pki/etcd/healthcheck-client.crt
 export ETCDCTL_KEY=/etc/kubernetes/pki/etcd/healthcheck-client.key
