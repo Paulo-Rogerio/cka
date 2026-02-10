3f0126bf91de595a66ff7634c5bdc113692b601c
Paulo Rogerio Gomes e Silva <prgomessilva@tjgo.jus.br>
2026-02-10 05:59:07 -0300

    Alterando binario de restore para etcdutl

--- CKA/02-etcd/etcd-manager/cluster/staticPod/restore/03-rescue-backup.sh
+++ CKA/02-etcd/etcd-manager/cluster/staticPod/restore/03-rescue-backup.sh
@@ -3,10 +3,7 @@
 # Assumindo que estou restaurando a partir do master03
 #
 etcdutl snapshot restore /backup/etcd-2026-02-10-0505.db \
-  --name=master03 \
-  --initial-cluster=master01=https://10.100.100.11:2380,master02=https://10.100.100.12:2380,master03=https://10.100.100.13:2380 \
-  --initial-cluster-state=new \
-  --initial-advertise-peer-urls=https://10.100.100.13:2380
+  --data-dir=/var/lib/etcd
 
 # Ajustar os manifests dos outros nós (initial-cluster-state=new)
 
