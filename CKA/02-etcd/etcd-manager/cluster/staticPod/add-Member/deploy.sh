9d2c5d73ef86988ce2f920113da376f8a71f4467
Paulo Rogerio Gomes e Silva <prgomessilva@tjgo.jus.br>
2026-02-12 06:18:55 -0300

    Add new member

---
+++ CKA/02-etcd/etcd-manager/cluster/staticPod/add-Member/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)
+
+source ./01-upgrade.sh
+source ./02-common.sh
+source ./03-etcdctl.sh
+source ./04-kubeadmcfg.sh
+source ./05-kubelet.sh
+source ./06-certificates.sh
+source ./07-notify.sh
