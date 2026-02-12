a7efb9d826df619cdf34bff719e3d135efaba32e
Paulo Rogerio Gomes e Silva <prgomessilva@tjgo.jus.br>
2026-02-12 06:29:16 -0300

    Correcoes

--- CKA/02-etcd/etcd-manager/data/01-env.sh
+++ CKA/02-etcd/etcd-manager/data/01-env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cat > /root/etcdctl.env <<EOF
-export ETCDCTL_ENDPOINTS=https://master01:2379,https://master02:2379
+export ETCDCTL_ENDPOINTS=https://master01:2379,https://master02:2379,https://master03:2379
 export ETCDCTL_CACERT=/etc/kubernetes/pki/etcd/ca.crt
 export ETCDCTL_CERT=/etc/kubernetes/pki/etcd/peer.crt
 export ETCDCTL_KEY=/etc/kubernetes/pki/etcd/peer.key
