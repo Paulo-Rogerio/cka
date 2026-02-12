44373c9f4ff192c79a0047cbbb85e9557a43c88e
Paulo Rogerio Gomes e Silva <prgomessilva@tjgo.jus.br>
2026-02-12 08:57:34 -0300

    Add registros

--- CKA/02-etcd/etcd-manager/data/05-insert-item.sh
+++ CKA/02-etcd/etcd-manager/data/05-insert-item.sh
@@ -5,6 +5,4 @@ source ./01-env.sh
 for i in {1..10}
 do
   etcdctl put "chave${i}" "value{i}"
-  etcdctl put "chave${i}" "value{i}"
-  etcdctl put "chave${i}" "value{i}"
 done
--- CKA/02-etcd/etcd-manager/data/06-read-item.sh
+++ CKA/02-etcd/etcd-manager/data/06-read-item.sh
@@ -4,6 +4,13 @@ source ./01-env.sh
 for i in {1..10}
 do
   etcdctl get chave{i} --prefix -w simple
+  echo "---------"
+done
+
+echo "################## JQ ####################"
+
+for i in {1..10}
+do
   etcdctl get chave{i} --prefix -w json | jq .
   echo "---------"
 done
