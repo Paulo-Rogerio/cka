f4527745e1a9b5f95da77984649fd07975eebd17
Paulo Rogerio Gomes e Silva <prgomessilva@tjgo.jus.br>
2026-02-12 09:04:02 -0300

    Add registros

--- CKA/02-etcd/etcd-manager/data/05-insert-item.sh
+++ CKA/02-etcd/etcd-manager/data/05-insert-item.sh
@@ -4,5 +4,5 @@ source ./01-env.sh
 
 for i in {1..10}
 do
-  etcdctl put "chave${i}" "value{i}"
+  etcdctl put "chave${i}" "value${i}"
 done
--- CKA/02-etcd/etcd-manager/data/06-read-item.sh
+++ CKA/02-etcd/etcd-manager/data/06-read-item.sh
@@ -3,14 +3,11 @@ source ./01-env.sh
 
 for i in {1..10}
 do
-  etcdctl get chave{i} --prefix -w simple
+  etcdctl get chave${i} -w simple
   echo "---------"
 done
 
 echo "################## JQ ####################"
 
-for i in {1..10}
-do
-  etcdctl get chave{i} --prefix -w json | jq .
-  echo "---------"
-done
+# Tudo que comecao com "chave"
+etcdctl get chave --prefix -w json | jq .
