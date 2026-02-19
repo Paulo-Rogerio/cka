184d8510150f1ca8ba95fa8dc0d7a7e9361d2554
Paulo Rogerio Gomes e Silva <prgomessilva@tjgo.jus.br>
2026-02-19 09:36:08 -0300

    Multi master

--- CKA/03-k8s/multiMaster/deploy.sh
+++ CKA/03-k8s/multiMaster/deploy.sh
@@ -6,15 +6,15 @@ cd $(dirname $0)
 if [[ $(hostname -s) == "master01" ]]
 then
   source ./01-common.sh
-  source ./master/vip.sh
-  source ./02-init.sh
+  source ./02-vip.sh
+  source ./03-init.sh
   source ../deployments/01-cni.sh
   source ../deployments/02-taint.sh
   source ../deployments/03-metallb.sh
   source ../deployments/04-metrics-server.sh
 else
   source ./01-common.sh
-  source ./02-init.sh
+  source ./03-init.sh
   source ../deployments/01-cni.sh
 fi
 
